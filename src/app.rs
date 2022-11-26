939c62edadc519f814c1dfaad79562c45833cd7c
João Vitor Silveira Ribeiro <joaovitorsrs7@gmail.com>
2022-11-26 12:09:22 -0300

    feat: now the app struct has a db attrbute

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,29 @@
-use std::time::Duration;
+use crate::db::Db;
+use anyhow::Result;
+use std::{path::PathBuf, time::Duration};
 
 #[derive(Debug)]
 pub struct App<'a> {
     pub title: &'a str,
     pub should_quit: bool,
     pub input_timeout: Duration,
+    pub db: Db,
 }
 
 impl<'a> App<'a> {
-    pub fn new(title: &'a str, input_timeout: Duration) -> App<'a> {
-        App {
+    pub fn new(
+        title: &'a str,
+        input_timeout: Duration,
+        custom_db_path: Option<PathBuf>,
+    ) -> Result<App<'a>> {
+        let db = Db::new(custom_db_path)?;
+        db.set_up_tables()?;
+        Ok(App {
             title,
             should_quit: false,
             input_timeout,
-        }
+            db,
+        })
     }
 
     pub fn on_key(&mut self, c: char) {
